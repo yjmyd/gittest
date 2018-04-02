a156ba0501274b9135824d40ff652e25daa3c43f
liudan <liudan@huimin100.cn>
2018-04-02 10:38:09 +0800

    Initial commit

---
+++ git-test/src/main/java/liudan/archetype/gittest/GitTestApplication.java
@@ -0,0 +1,12 @@
+package liudan.archetype.gittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTestApplication.class, args);
+	}
+}
