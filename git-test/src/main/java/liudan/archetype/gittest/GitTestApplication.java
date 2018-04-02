57876684190bbfb21056610e33c24f2f77fd1a43
liudan <liudan@huimin100.cn>
2018-04-02 11:32:54 +0800

    测试提交

--- git-test/src/main/java/liudan/archetype/gittest/GitTestApplication.java
+++ git-test/src/main/java/liudan/archetype/gittest/GitTestApplication.java
@@ -8,6 +8,7 @@ public class GitTestApplication {
 
 	public static void main(String[] args) {
 		System.out.println("小张同学增加了一行代码，用于测试git提交测试。");
+		System.out.println("我是分支提交的代码");
 		SpringApplication.run(GitTestApplication.class, args);
 	}
 }
